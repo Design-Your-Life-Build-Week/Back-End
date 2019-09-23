a2120c988828528b7b93a83c8a36719e0bfef308
Adam Hoffman <wintros@yahoo.com>
2019-09-23 12:36:38 -0600

    pg

--- knexfile.js
+++ knexfile.js
@@ -19,12 +19,9 @@ module.exports = {
   },
   production: {
     client: 'pg',    
-    connection: 'process.env.DATABASE_URL',
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
-      }
-    },
+    connection: process.env.DATABASE_URL,
+    
+    
     migrations: {
       directory: './data/migrations'
     }
