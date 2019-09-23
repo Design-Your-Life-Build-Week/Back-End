019b4d35b05124d103b59c78ec9872af93978a96
Adam Hoffman <wintros@yahoo.com>
2019-09-23 11:24:35 -0600

    login endpoint

--- users/users-router.js
+++ users/users-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const Users = require('./users-model.js')
 const router = express.Router();
+const jwt = require('jsonwebtoken')
 
 
 
@@ -13,6 +14,36 @@ router.post('/register', (req, res) => {
         res.send(registeredUser)
     })
 });
+
+router.post('/login', (req, res) =>{
+    let {username, password} = req.body
+    Users.findBy({username})
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user)
+            res.status(200).json({message: `Welcome ${user.username}!`, token})
+        } else {
+            res.status(401).json({message: "This is not a registered user"})
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
+function generateToken(user) {
+    const payload = {
+        username: user.username
+    }
+    const secret = "keep it secret, keep it safe"
+    const options = {
+        expiresIn: '1d'
+    }
+    return jwt.sign(payload, secret, options)
+}
+
+
 
 
 
